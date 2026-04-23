e0107595341de62c13f78c2dc8461dc779712d01
Laila2345 <110055434+Laila2345@users.noreply.github.com>
2026-04-23 01:31:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     # Vous pouvez changer ce texte pour personnaliser votre TP
-    return "Bienvenue sur mon application CI/CD - Version 1.0"
+    return "Version 2 - Déploiement automatique avec Poll SCM!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
